d596b1143ff9df47c7fff89d62a7fb074f93c519
mistifikatorcat <92595353+mistifikatorcat@users.noreply.github.com>
2022-12-27 18:40:45 +0200

    fix 768

--- src/components/SavedHeader/SavedHeader.js
+++ src/components/SavedHeader/SavedHeader.js
@@ -8,7 +8,7 @@ function SavedNewsHeader({username}){
         <section className="saved-header">
             <div className="saved-header__wrapper">
                <p className="saved-header__label">Saved Articles</p>
-               <h2 className="saved-header__title">{username}, you have {initialData.length}  saved articles</h2> 
+               <h1 className="saved-header__title">{username}, you have {initialData.length} saved articles</h1> 
                <p className="saved-header__keywords">By keywords:
                <span className='saved-header__keyword'> {initialData[0].keyword}, {initialData[1].keyword}, and {initialData.length-2} more </span></p>
             </div>
