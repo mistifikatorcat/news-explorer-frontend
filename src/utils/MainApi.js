d3935ee55a868063365f0340468c0cc880dd36b0
mistifikatorcat <92595353+mistifikatorcat@users.noreply.github.com>
2023-01-03 18:00:39 +0200

    add newsapi, add mainapi, add auth

---
+++ src/utils/MainApi.js
@@ -0,0 +1,86 @@
+export class MainApi {
+    constructor({ baseUrl, headers }) {
+      this._baseUrl = baseUrl;
+      this._headers = headers;
+    }
+  
+    _respond(res) {
+        return res.ok
+          ? res.json()
+          : Promise.reject(`Something went wrong: ${res.status}`)
+      ;
+    }
+  
+    getUserInfo() {
+      return fetch(`${this._baseUrl}/users/me`, {
+        headers: {
+          "Content-Type": "application/json",
+          authorization: `Bearer ${localStorage.getItem("jwt")}`,
+        }
+      }).then(this._respond);
+    }
+  
+    getArticles() {
+      return fetch(`${this._baseUrl}/articles`, {
+        headers: {
+          "Content-Type": "application/json",
+          authorization: `Bearer ${localStorage.getItem("jwt")}`,
+        }
+      }).then(this._respond);
+    }
+  
+    saveArticle(article) {
+        return fetch(`${this._baseUrl}/articles/`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            authorization: `Bearer ${localStorage.getItem("jwt")}`,
+          },
+          body: JSON.stringify(article),
+        }).then(this._respond);;
+      }
+
+
+    deleteArticle(id) {
+      return fetch(`${this._baseUrl}/articles/${id}`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+          authorization: `Bearer ${localStorage.getItem("jwt")}`,
+        },
+      }).then(this._respond);;
+    }
+  }
+  
+  //initializing api
+  
+  
+  let node_env = 'production';
+  
+  let baseUrl = 'http://localhost:3001' 
+  // node_env === 'production'
+//   ? 'https://api.articlemonkeys.students.nomoredomainssbs.ru'
+//   : 'https://articlemonkeys.students.nomoredomainssbs.ru';
+  
+  
+  //let baseUrl = 'https://api.articlemonkeys.students.nomoredomainssbs.ru';
+  
+  
+  const mainApi = new MainApi({
+   baseUrl
+  });
+  
+  export default mainApi;
+  
+  
+  /*const api = new Api({
+    baseUrl: "https://around.nomoreparties.co/v1/cohort-3-en",
+    headers: {
+      authorization: "b451294b-44d9-464a-8874-2d4137a4eb3c",
+      "Content-Type": "application/json",
+    },
+  });
+  
+  export default api;
+  */
+  
---
+++ src/utils/NewsApi.js
@@ -0,0 +1,38 @@
+class NewsApi{
+    constructor({url, key}){
+        this.url = url;
+        this.key = key;
+        this.time = new Date();
+    }
+
+
+    _respond(res) {
+        return res.ok
+          ? res.json()
+          : Promise.reject(`Something went wrong: ${res.status}`)
+      ;
+    }
+
+    _getWeek(){
+        const today = new Date();
+        let dateRange = new Date();
+
+        dateRange = dateRange.setDate(today.getDate()-7)
+
+        return dateRange;
+    }
+
+    getArticles(keyword){
+        const getWeek = this._getWeek();
+        return fetch(`${this.url}?q=${keyword}&apiKey=${this.key}&from=${getWeek}&to=${this.time}&pageSize=100`)
+        .then(this._respond);
+    }
+
+}
+
+const newsApi = new NewsApi({
+    url: 'https://nomoreparties.co/news/v2/everything',
+    key: 'c7556585b6ca4cbe805fdb777cbbc7b4' 
+})
+
+export default newsApi;
