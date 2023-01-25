bfa6727eb0da8407c12f22034d6d7d3cf08d2cf5
mistifikatorcat <92595353+mistifikatorcat@users.noreply.github.com>
2023-01-25 23:24:17 +0200

    small fix

--- src/components/InfoToolTip/InfoToolTip.js
+++ src/components/InfoToolTip/InfoToolTip.js
@@ -4,11 +4,17 @@ import "./infotooltip.css";
 
 export default function InfoToolTip({ isOpen, onClose, onLoginClick }) {
   const enable = `${isOpen ? "popup_enabled" : ""}`;
+
+  const handleLoginClick = () => {
+    onClose();
+    onLoginClick();
+  };
+
   return (
     <section className={`tooltip popup ${enable}`}>
       <button className="tooltip__close" onClick={onClose}></button>
       <h2 className="tooltip__header">Registration successfully completed!</h2>
-      <Link className="tooltip__redirect" onClick={onLoginClick}>
+      <Link className="tooltip__redirect" onClick={handleLoginClick}>
         Sign In
       </Link>
     </section>
